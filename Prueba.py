5c988def1651b184820435c92b100ca348d03d35
Ryuudarkwarrior <barco797@gmail.com>
2024-06-01 13:16:28 +0200

    Add files via upload

---
+++ Prueba.py
@@ -0,0 +1,9 @@
+from playsound import playsound
+from PIL import Image
+
+img = Image.open(r"C:\Users\Ryuu\Desktop\Quevedo_(copia_de_Velázquez).png")
+img.show()
+
+playsound(r"C:\Users\Ryuu\Desktop\QUEVEDO  BZRP Music Sessions #52.mp3") 
+
+
